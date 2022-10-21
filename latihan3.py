db8927a1375023f764f0e82e567b36ad11e95f2e
hapiyansyah <m.hapiyansyah1@gmail.com>
2022-10-21 10:18:29 +0700

    script latihan3

--- latihan3.py
+++ latihan3.py
@@ -7,7 +7,7 @@ print("Variable a=",a)
 print("Variable b=",b)
 
 # cetak hasil operasi kedua variabel dengan String Format
-print("Hasil Penggabungan (1)&(0)=%".format(a,b)%(a+b))
+print("Hasil Penggabungan {1}&{0}=%".format(a,b)%(a+b))
 
 # konversi nilai variabel
 a = int(a)
