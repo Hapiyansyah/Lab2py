597de8af7b467fc90f257ff6bcddaf8321711872
hapiyansyah <m.hapiyansyah1@gmail.com>
2022-10-21 11:08:38 +0700

    latihan3

--- latihan3.py
+++ latihan3.py
@@ -1,16 +1,16 @@
 # input nilai variabel
-a = input("masukkan nilai a")
-b = input("masukkan nilai b")
+a = input("masukkan nilai a: ")
+b = input("masukkan nilai b: ")
 
 # cetak nilai variabel
 print("Variable a=",a)
 print("Variable b=",b)
 
 # cetak hasil operasi kedua variabel dengan String Format
-print("Hasil Penggabungan {1}&{0}=%".format(a,b)%(a+b))
+print("Hasil Penggabungan", a, '&', b, '=', "{0}{1}".format (a,b))
 
 # konversi nilai variabel
 a = int(a)
 b = int(b)
-print("Hasil penjumlahan {1}+{0}=%d".format(a,b)%(a,b))
-print("Hasil penjumlahan {1}/{0}=%d".format(a,b)%(a/b))
+print("Hasil penjumlahan", a,'+',b,"= %d" %(a+b))
+print("Hasil penjumlahan", a,'/',b,"= %d" %(a/b))
